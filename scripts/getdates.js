79de1b924cf3a10e84a903ca9010d47cc6671fbb
Dani <das22001@byui.edu>
2025-05-16 22:48:48 +0200

    commit

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,8 +1,8 @@
-// Set current year
+
 const year = new Date().getFullYear();
 document.querySelector("#currentyear").textContent = year;
 
-// Set last modified date
+
 const lastModified = document.lastModified;
 document.querySelector("#lastModified").textContent = `Last Update: ${lastModified}`;
 
